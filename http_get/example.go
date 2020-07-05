982563dbd244a946c35135dbef39217cb730e477
HJSang <sanghj0923@gmail.com>
2020-07-05 15:09:47 -0400

    update

--- http_get/example.go
+++ http_get/example.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	url := "https://www.google.com/search?q=google&sxsrf=ALeKk02mc4588E4c2tvgqlEkpjPox7Gh_w:1593741086976&source=lnms&tbm=nws&sa=X&ved=2ahUKEwirlema_K_qAhW-hHIEHROjC-sQ_AUoAXoECB8QAw&biw=1440&bih=820" 
+	url := "https://en.wikipedia.org/wiki/IMDb"
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
